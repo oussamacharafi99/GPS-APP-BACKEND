2183e44a049b58a2369ede747811c6b04528a44f
Your Name <you@example.com>
2025-08-11 16:48:10 +0100

    I added the project structure !

---
+++ src/main/java/GPS/CO/CoGpsApplication.java
@@ -0,0 +1,13 @@
+package GPS.CO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoGpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoGpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GPS/CO/CoGpsApplicationTests.java
@@ -0,0 +1,13 @@
+//package GPS.CO;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class CoGpsApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
